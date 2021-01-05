31641993cfcff4c605ecc32680394301f4395291
DawiT <Dawid@Tararuj.be>
2021-01-05 11:28:36 +0100

    exo 3 reste

---
+++ public/js/main.js
@@ -0,0 +1,131 @@
+/*
+
+let a = 1; 
+let b = 2; 
+let c = 3; 
+
+console.log(a,b,c);
+
+a = a ^ c; 
+b = c;
+c = a ^ b; 
+
+console.log(a,b,c);
+
+*/
+
+
+
+/*
+let a = 1;
+let b = 2; 
+let c = 3;
+let d = 4; 
+
+let temp; 
+
+temp = a; temp2 = b; 
+a = d; 
+b = c; 
+c = temp2; 
+d = temp; 
+console.log(a,b,c,d);
+
+*/
+
+
+
+/*
+let a = 1,b = 2, c = 3, d = 4; 
+console.log(a,b,c,d);
+
+temp = a; 
+a = b; 
+b = c; 
+c = d; 
+d = temp;
+
+console.log(a,b,c,d);
+
+*/
+
+
+
+/*
+
+let tableau = [1, 2, 3, 4];
+
+let renverse = tableau.reverse(); 
+
+console.log(renverse); 
+
+*/
+
+ // pas fait ;;; 
+
+
+ 
+let a = 1, 
+    b = 2, 
+    c = 3,
+    d = 4,
+    e = 5, 
+    f = 6,
+    t = a; 
+
+console.log(a,b,c,d,e,f, "\n4 5 1 2 6 3 "); 
+
+a = d; 
+d = b; 
+b = e; 
+c = t;
+
+e = f; 
+f = c; 
+console.log(a,b,c,d,e,f);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+t2 = d; 
+t3 = e; 
+
+a = t2; 
+b = t3; 
+
+t = f; 
+f = c;
+e = t; 
+
+t = d; 
+d = b;  
+
+t = a; 
+c = a; 
+
+
+console.log(a,b,c,d,e,f); 
+
+
+
+*/
